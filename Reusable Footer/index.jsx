3fe1b7368a9fa2307f4aa6fc29a65a95a37864a9
Preksha Barjatya <140542863+Prekshabarjatya@users.noreply.github.com>
2025-09-20 08:07:18 +0530

    Create index.jsx

---
+++ Reusable Footer/index.jsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms & Conditions</a></li>
+      </ul>
+      <p>© 2025 Your Preksha. All rights reserved.</p>
+      <a href="#">Facebook</a>
+      <a href="#">Twitter</a>
+      <a href="#">LinkedIn</a>
+    </footer>
+  )
+};
